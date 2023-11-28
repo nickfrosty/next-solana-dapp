64a9d68fda5b84f0f69de820c1dd1508051548f8
nickfrosty <nfrostbutter@gmail.com>
2023-11-28 17:26:57 -0500

    refactor: removed unused import

--- web/app/provider.tsx
+++ web/app/provider.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { ReactNode, useState } from 'react';
+import { ReactNode, useState } from 'react';
 import { ReactQueryStreamedHydration } from '@tanstack/react-query-next-experimental';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 
